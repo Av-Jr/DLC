2ace9a93bca9e9a90fab0173fefe1634c8cd0923
Aryansh V <emailtoav09@gmail.com>
2026-04-14 02:17:11 +0530

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import "./App.css"
+import HP_main from"./HomePage/HP_main.jsx"
+
+const App = () => {
+  return(<>
+    <HP_main></HP_main>
+  </>);
+}
+
+export default App;
---
+++ src/HomePage/HP_main.jsx
@@ -0,0 +1,8 @@
+
+const HP_main = () => {
+    return(<>
+
+    </>)
+}
+
+export default HP_main;
