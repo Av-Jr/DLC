743659197efaeafb999a7b51721b1dec51c324e7
Aryansh V <emailtoav09@gmail.com>
2026-04-16 23:09:21 +0530

    designed the Ham menu for mobile viewport with responsive Nav bar

--- src/HomePage/NavBar_Desktop.jsx
+++ src/HomePage/NavBar_Desktop.jsx
@@ -2,18 +2,22 @@ import {useState} from 'react'
 import './NavBarDesk.css'
 
 function NavBar_Desktop(){
+    const [HamMenu, setHam] = useState(false);
+
+    const setHamval = () => {setHam(!HamMenu)}
     return (
         <>
+            <div className={`overlay ${HamMenu ? 'active' : ' '}` } onClick = {setHamval}></div>
             <div className="Nav">
                 <img src="/logo.png" alt="" className="NavLogo"/>
 
-                <div className="ham">
-                    <span className="hamL"></span>
-                    <span className="hamL"></span>
-                    <span  className="hamL"></span>
+                <div className="ham" onClick = {setHamval}>
+                    <span className={`hamL ${HamMenu ? 'active' : ' '}`}></span>
+                    <span className={`hamL ${HamMenu ? 'active' : ' '}`}></span>
+                    <span className={`hamL ${HamMenu ? 'active' : ' '}`}></span>
                 </div>
 
-                <section className="con2">
+                <section className={`con2 ${HamMenu ? 'active' : ' '}`}>
                     <div className="eleCon">
                         <ul className="links">
                             <li className="linkEle">We <span className="arrow">⌄</span></li>
